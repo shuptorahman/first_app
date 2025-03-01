a421037ad4faba09167afa23faea0fe8d205636d
Shupto Rahman <shuptorahman8@gmail.com>
2025-03-01 19:40:25 +0600

    custom widget created

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(255, 3, 38, 57),
-                Color.fromARGB(255, 16, 104, 152),
-              ],
-            ),
-          ),
-          child: Center(
-            child: Text(
-              'Shupto Rahman',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
+    MaterialApp(home: Scaffold(body: GradientContainer())),
+  );
+}
+
+//creating custom widget.*custom widget nothing but a bunch of widgets
+//that are grouped together to create a new widget.
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 3, 38, 57),
+            Color.fromARGB(255, 16, 104, 152),
+          ],
+        ),
+      ),
+      child: Center(
+        child: Text(
+          'Welcome',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
