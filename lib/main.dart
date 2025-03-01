be44b2bf75d822f80d123b4070d474e147ea4f58
Shupto Rahman <shuptorahman8@gmail.com>
2025-03-01 19:14:11 +0600

    gradient bg added,textStyle added-practicing

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Shupto'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 3, 38, 57),
+                Color.fromARGB(255, 16, 104, 152),
+              ],
+            ),
+          ),
+          child: Center(
+            child: Text(
+              'Shupto Rahman',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
         ),
       ),
     ),
